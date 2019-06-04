41455c2d564c39d2ccfd92dfd91ac0f3dd60dbdc
SIMDD <dev.simdd@gmail.com>
2019-06-04 21:15:11 +0800

    fix: 🐛 get userinfo

--- parse.py
+++ parse.py
@@ -1,21 +1,23 @@
 import re
-import os
-import readline
 
 with open('demo.ppts', 'r') as file:
     text = file.read()
 
-reCover = r"--cover--(.*)--cover--"
-rePages = r"--page--(.*)--page--"
-reTitle = r"title:\s*([^\n\r]*)\n"
-reAuthor = r"author:\s*([^\n\r]*)\n"
-reDate = r"date:\s*([^\n\r]*)\n"
 
-cover = re.search(reCover, text, re.S).group(1)
-title = re.search(reTitle, cover, re.S).group(1)
-author = re.search(reAuthor, cover, re.S).group(1)
-date = re.search(reDate, cover, re.S).group(1)
+def parseInfo():
+    reCover = r"--cover--(.*)--cover--"
+    reTitle = r"title:\s*([^\n\r]*)\n"
+    reAuthor = r"author:\s*([^\n\r]*)\n"
+    reDate = r"date:\s*([^\n\r]*)\n"
 
-print(title)
-print(author)
-print(date)
+    cover = re.search(reCover, text, re.S).group(1)
+    title = re.search(reTitle, cover, re.S).group(1)
+    author = re.search(reAuthor, cover, re.S).group(1)
+    date = re.search(reDate, cover, re.S).group(1)
+
+    return {title, author, date}
+
+
+userinfo = parseInfo()
+
+print(userinfo)
