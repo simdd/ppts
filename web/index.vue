62372ae4eebf1006a9ecfdee0f6d8c09f7e0a287
SIMDD <dev.simdd@gmail.com>
2019-06-06 20:26:26 +0800

    feat: 🎸 web

---
+++ web/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="a">
+    <v-slide></v-slide>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Slide from "./slide.vue";
+
+export default {
+  name: "app",
+
+  components: {
+    "v-slide": Slide
+  }
+};
+</script>
+
+<style lang="less"></style>
---
+++ web/slide.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page">
+    <section v-if="idx < 0">
+      <p class="label">{{user.title}}</p>
+      <p class="label">{{user.author}}</p>
+      <p class="label">{{user.date}}</p>
+    </section>
+    <section v-else>{{slide[idx]}}</section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      idx: -1,
+      user: {
+        title: "这里是名字",
+        author: "simdd",
+        date: "2019/02/10"
+      },
+      slide: ["page1", "page2"]
+    };
+  },
+
+  mounted() {
+    console.log(1);
+    document.addEventListener("keydown", e => {
+      if (e.keyCode === 32) {
+        if (this.idx >= this.slide.length - 1) {
+          this.idx = -1;
+        } else {
+          this.idx = this.idx + 1;
+        }
+      }
+    });
+  }
+};
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
+
+<style lang="less" scoped>
+.page {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .label {
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+}
+</style>
