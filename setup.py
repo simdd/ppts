9c9c56c14643415ef32878ff4a04f27d490ac860
SIMDD <dev.simdd@gmail.com>
2019-06-10 13:02:37 +0800

    feat: 🎸 publich pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ppts',
+    version='1.0.0',
+    description='ppt with web',
+    author='simdd',
+    author_email='dev.simdd@gmail.com',
+    packages=find_packages(),
+    scripts=['cli/bin.py']
+)
